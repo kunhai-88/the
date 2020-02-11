b0c8dac286ada03bab2aacbc7d7ccc027cb7c229
杨坤海 <384037191@qq.com>
2020-02-11 18:51:56 +0800

    ui

--- scripts/parse.js
+++ scripts/parse.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 const words = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/words.json'), 'utf8'));
-let markdown = '# 常用的单词 \n'
+const html = fs.readFileSync(path.join(__dirname, '../template.html'), 'utf8');
+let markdown = ''
 for(let i=0;i< words.length; i++){
-  markdown = markdown + `<p>${i+1}. <a href="https://fanyi.baidu.com/#en/zh/${words[i].word}" target="_blank" >${words[i].word}</a> (${(words[i].frequency*100).toFixed(4)}%) </p>\n`;
+  markdown = markdown + `<a class="word"  onClick="query('${words[i].word}', this)" ><span>${i+1}. ${words[i].word}</span> <span class="frequency">${(words[i].frequency*100).toFixed(4)}%</span> </a>\n`;
 }
  
+// fs.writeFileSync(
+//   path.resolve(__dirname, '../', 'README.Md'),
+//   markdown,
+// );
+
+
 fs.writeFileSync(
-  path.resolve(__dirname, '../', 'README.Md'),
-  markdown,
+  path.resolve(__dirname, '../docs', 'index.html'),
+  html.replace('{{words}}', markdown),
 );
-
-
