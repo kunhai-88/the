5a679eea9ca61b75644ce6060bfd805abdc54b02
杨坤海 <384037191@qq.com>
2020-02-11 15:05:37 +0800

    样式修改

--- scripts/parse.js
+++ scripts/parse.js
@@ -4,7 +4,7 @@ const path = require('path');
 const words = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/words.json'), 'utf8'));
 let markdown = '# 常用的单词 \n'
 for(let i=0;i< words.length; i++){
-  markdown = markdown + `- ${i+1} - [${words[i].word}](https://fanyi.baidu.com/#en/zh/${words[i].word})  (${(words[i].frequency*100).toFixed(4)}%) \n`;
+  markdown = markdown + `<p>${i+1}. <a href="https://fanyi.baidu.com/#en/zh/${words[i].word}" target="_blank" >${words[i].word}</a> (${(words[i].frequency*100).toFixed(4)}%) </p>\n`;
 }
  
 fs.writeFileSync(
