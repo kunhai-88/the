e96b6e6db8a4d123a876ee5b8cfc34941c877db0
杨坤海 <384037191@qq.com>
2020-02-12 13:30:48 +0800

    add logo

--- scripts/parse.js
+++ scripts/parse.js
@@ -5,7 +5,7 @@ const words = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/words.jso
 const html = fs.readFileSync(path.join(__dirname, '../template.html'), 'utf8');
 let markdown = ''
 for(let i=0;i< words.length; i++){
-  markdown = markdown + `<a class="word"  onClick="query('${words[i].word}', this)" ><span>${i+1}. ${words[i].word}</span> <span class="frequency">${(words[i].frequency*100).toFixed(4)}%</span> </a>\n`;
+  markdown = markdown + `<a class="word ${i===0 ? 'active':''}"  onClick="query('${words[i].word}', this)" ><span>${i+1}. ${words[i].word}</span> <span class="frequency">${(words[i].frequency*100).toFixed(4)}%</span> </a>\n`;
 }
  
 // fs.writeFileSync(
